2bbace673547d62a818b0adb4f8b1fe0dd2976a2
Oleh <gend4016@gmail.com>
2023-09-19 14:52:55 +0300

    controller

--- src/api/brand/controllers/brand.js
+++ src/api/brand/controllers/brand.js
@@ -1,9 +1,21 @@
+// Ваш контроллер brand.js
 'use strict';
-
-/**
- * brand controller
- */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::brand.brand');
+module.exports = createCoreController('api::brand.brand', {
+  async find(ctx) {
+    const { request } = ctx;
+    const filters = request.query.filters || {};
+
+    // Проверяем, есть ли параметр category.id в фильтрах
+    if (filters['category.id']) {
+      // Если есть, фильтруем бренды по id категории
+      const categoryId = filters['category.id'];
+      return strapi.services.brand.find({ 'category.id': categoryId });
+    } else {
+      // Если параметр category.id отсутствует, возвращаем все бренды
+      return strapi.services.brand.find({});
+    }
+  },
+});
